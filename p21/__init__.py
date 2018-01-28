f7beb332dac4e2d90da029647c7075488a801b08
elidioxg <elidioxg@gmail.com>
2018-01-28 03:19:14 -0300

    First files

---
+++ p21/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def classFactory(iface):
+    from p21_plugin import P21
+    return P21(iface)
---
+++ p21/p21_plugin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from PyQt4.QtCor import *
+from PyQt4.QtGui import *
+from qgis.core import *
+
+class P21:
+
+    def __init__(self, iface):
+        self.iface = iface
+
+    def initGui(self):
+        pass
+
+    def unload(self):
+        pass
+
+    def run(self):
+        pass
